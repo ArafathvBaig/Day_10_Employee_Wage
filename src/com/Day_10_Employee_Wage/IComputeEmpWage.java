97d8ccce44d88ce8e41d52f550b7c2cbb9ce2835
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-11-03 15:06:01 +0530

    Updated: EmployeeWage.java File With UC11

---
+++ src/com/Day_10_Employee_Wage/IComputeEmpWage.java
@@ -0,0 +1,10 @@
+package com.Day_10_Employee_Wage;
+
+public interface IComputeEmpWage 
+{
+	public void addCompanyEmpWage(String company, int empRatePerHour,
+			int maxWorkingDays, int maxWorkingHours);
+	public void computeEmpWage();
+	public int computeEmpWage(CompanyEmpWage companyEmpWage);
+	public int getTotalWage(String company);
+}
